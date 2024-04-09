a2d7f0a31ee414848df5665fcc5751adc24910ec
TZevs <c3031659@hallam.shu.ac.uk>
2024-04-09 16:27:56 +0100

    suspend done

--- customers.php
+++ customers.php
@@ -8,9 +8,38 @@
     $userEmail = $_SESSION['userEmail'];
  
     require_once("includes/db_conn.php");
-    $customerInfo = "SELECT * FROM customeraccounts
-                        INNER JOIN currencywallet ON currencywallet.customer_id = customeraccounts.customer_id"; 
-    $info_results = $conn->query($customerInfo);
+    $customerInfo = "SELECT * FROM customeraccounts"; 
+    $info_results = mysqli_query($conn, $customerInfo);
+
+    if(isset($_POST["submit"])) {
+        $id = $_POST["customerid"];
+        $toggleSuspension = $_POST["suspend"]; 
+        
+        $toggle = mysqli_fetch_assoc($info_reuslts);
+        $isSuspended = $toggle['suspension'];
+
+        if (isset($toggleSuspension)) {
+            if ($isSuspended == "True") {
+                $updateSuspend = "UPDATE customeraccounts SET suspension='False' WHERE customer_id = '$id'";
+                if ($conn->query($updateSuspend) === TRUE) {
+                    echo "<div class='alert alert-success'>Update Successful. <a href='customers.php'>Refresh.</a></div>";
+                } else {
+                    die("Something went wrong.");
+                }
+            } else if ($isSuspended == "False") {
+                $updateSuspend = "UPDATE customeraccounts SET suspension='True' WHERE customer_id = '$id'"; 
+                if ($conn->query($updateSuspend) === TRUE) {
+                    echo "<div class='alert alert-success'>Update Successful. <a href='customers.php'>Refresh.</a></div>";
+                } else {
+                    die("Something went wrong.");
+                }
+            } else {
+                echo "<div class='alert alert-danger'>Something went wrong.</div>";
+            }
+        }
+        
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -38,33 +67,28 @@
         </header>
         <div class="container">
             <h2 class="text-center">Customers</h2>
+            <form action="customers.php" method="POST">
             <?php
                 while ($obj = $info_results->fetch_object()) {
                     echo "<div class='card'>";
-
                         echo "<div class='card-header'>";
-                        echo "Customer ID: {$obj->customer_id}";
-
-                        echo "<form action='' method='POST'>
-                            <div class='form-group'>
-                                <label for='suspend'>Suspension Status:</label>
-                                <select name='suspend' id='suspend' class='class-control'>
-                                    <option value='False' selected>False</option>
-                                    <option value='True'>True</option>
-                                </select>
-                                <input type='submit' value='Suspend Account' class='btn btn-dark btn-sm'>
-                            </div>
-                        </form>";
+                            echo "<h5>Customer ID: {$obj->customer_id}</h5>";
+                            echo "<p>Is Suspended: {$obj->suspension}</p>";
+                            echo "<input type='checkbox' name='suspend' id='suspend' class='check-input'>";
+                            echo "<label for='suspend'>Toggle Suspension</label>";
                         echo "</div>";
-
                         echo "<div class='card-body'>";
-                            echo "<h5>Name: {$obj->first_name} {$obj->middle_name} {$obj->last_name}</h5>";
-                            echo "<h6>Email Address: {$obj->email_address}</h6>";
+                            echo "<p>Name: {$obj->first_name} {$obj->middle_name} {$obj->last_name}</p>";
+                            echo "<p>Email Address: {$obj->email_address}</p>";
                         echo "</div>";
-
+                        echo "<div class='card-footer'>
+                                <input type='number' Placeholder='Confirm Customer ID' name='customerid' class='form-control'>
+                                <input type='submit' value='Update' class='btn btn-primary btn-sm'>
+                            </div>";
                     echo "</div>";
                 }
             ?>
+            </form>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
